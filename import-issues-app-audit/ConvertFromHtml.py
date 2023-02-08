c20c24e3001009bae20e3e81da6040fa1627d286
Roland Campbell <roland.campbell@gmail.com>
2023-02-08 12:39:44 -0500

    working on it

--- import-issues-app-audit/ConvertFromHtml.py
+++ import-issues-app-audit/ConvertFromHtml.py
@@ -1,7 +1,7 @@
 import sys
 import argparse
 import csv
-# import panda
+
 import os.path
 from os import path
 try: 
@@ -14,15 +14,15 @@ class ImportFile:
     fileName = ""
     platform = ""
     def __init__(self, fileName, platform):
-        self.fileName = args.fileName
-        self.platform = args.platform
+        self.fileName = fileName
+        self.platform = platform
         print("in constructor")
     def convertFile(self):
         firstTime = True
         print("convert file")
         link = open(self.fileName)
         writeFile = open(".\\fixedfile.csv", "w")
-        soup = BeautifulSoup(link.read(), features="lxml")
+        soup = BeautifulSoup(link.read(),features="html.parser")    
         soup.prettify
         print(soup.title)
         if firstTime:
@@ -32,28 +32,20 @@ class ImportFile:
             firstTime = False
         
         print("not first time")
-        areaWrappers = soup.find_all("div", class_="area-wrapper")
-   
+          
 
-        for areaWrapper in areaWrappers:
-            # print(areaWrapper)
-            print('\n')
-            aqaLinks = areaWrapper.find_all('a', href = True)
-            aqaDescs = areaWrapper.find_all("section", class_="markdown")
-            aqaTitle = areaWrapper.find_all("h6")
-            aqaName = areaWrapper.find_all("h5")
-            writeFile = open(".\\fixedfile.csv", "a")
+        for table in soup.find_all('table', {'class': 'comments-table'}):
+            print(table)  
+            print(table.find('th').text) 
+                
+
+            p = table.find('p', {'class': 'comment-body'}).text
+            print(p)
 
             
-            for i in range(len(aqaLinks)):
-                link = str(aqaLinks[i]['href'])
-                descs = str(aqaDescs[i].find('p').getText())
-                title = str(aqaTitle[i].getText())
-                name = str(aqaName[i].getText())
-                writeFile.write(("bug,\"" + name + " " + title + "\",,\"" + link + "," + descs + "\"\n"))
-                print(("bug,\"" + name + " " + title + "\",,\"" + link + "," + descs + "\"\n"))
 
-            print("   \n")
+
+       
         writeFile.close
 
            
@@ -64,15 +56,18 @@ class ImportFile:
         
 
 
+def main():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('-p', action="store", dest="platform", choices=["jira"],default="jira", help=("only jira suported at this time"))
+    parser.add_argument('-f', action="store", dest="fileName", required=True)   
+    args = parser.parse_args()
+    if not path.exists(args.fileName):
+        print("file " + str(args.fileName) + "doesn't exist")
+        sys.exit(1)
+    updateFile = ImportFile(args.fileName, args.platform)
+    print (updateFile.platform)
+    updateFile.convertFile()
+    print ("after convert")
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument('-p', action="store", dest="platform", choices=["jira"],default="jira", help=("only jira suported at this time"))
-parser.add_argument('-f', action="store", dest="fileName", required=True)   
-args = parser.parse_args()
-if not path.exists(args.fileName):
-	print("file " + str(args.fileName) + "doesn't exist")
-	sys.exit(1)
-updateFile = ImportFile(args.fileName, args.platform)
-print (updateFile.platform)
-updateFile.convertFile()
-print ("after convert")
+if __name__ == "__main__":
+    main()
